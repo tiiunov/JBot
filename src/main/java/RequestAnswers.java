60c076fdf6003b512e0f8116cfe547c21f1719a1
tiiunov <65350634+tiiunov@users.noreply.github.com>
2020-12-06 22:24:17 +0500

    Update and rename Request_answers.java to RequestAnswers.java

--- src/main/java/RequestAnswers.java
+++ src/main/java/RequestAnswers.java
@@ -1,4 +1,4 @@
-public class Request_answers {
+public class RequestAnswers {
     static final String HELP = "Привет! Я знаю все о погоде." + "\n" +
             "- Напиши /weather если хочешь узнать, сколько градусов сейчас за окном." + "\n" +
             "- Напиши /clothing\\_advice если тебе нужно помочь одеться по погоде:)";
